5cf99cede5650b98a5093b8ffb1fbfaf5a22f546
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-11-14 11:52:47 +0100

    done for now

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import Header from '../../component/Header'; // <- importer
+import Header from '../../component/Header'; 
 
 const Home = ({ onStartGame, onBackHome, onLogin }) => {
   return (
